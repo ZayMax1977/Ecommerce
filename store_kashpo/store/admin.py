006efc4ac171db6da4019ad3482c2e8c3c79b577
Max Zanchenko <zamax77@mail.ru>
2023-10-18 16:56:18 +0300

    в админке Inline, новое поле в Order - sent

--- store_kashpo/store/admin.py
+++ store_kashpo/store/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import *
 
+class OrderInline(admin.TabularInline):
+    model = Order
+    extra = 0
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.fields]
+    inlines = [OrderItemInline]
+    class Meta:
+        model = Order
+admin.site.register(Order,OrderAdmin)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Customer._meta.fields]
+    inlines = [OrderInline]
+    class Meta:
+        model = Customer
+admin.site.register(Customer,CustomerAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id','name']
@@ -8,20 +29,20 @@ class CategoryAdmin(admin.ModelAdmin):
         model = Category
 admin.site.register(Category,CategoryAdmin)
 
-
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['id','product','color','order','quantity']
-    class Meta:
-        model = OrderItem
-admin.site.register(OrderItem,OrderItemAdmin)
-
-
 class ShippingAddressAdmin(admin.ModelAdmin):
     list_display = [field.name for field in ShippingAddress._meta.fields if field.name != "id"]
     class Meta:
         model = ShippingAddress
 admin.site.register(ShippingAddress,ShippingAddressAdmin)
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+    class Meta:
+        model = Product
+admin.site.register(Product,ProductAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['id','product','color','order','quantity']
+    class Meta:
+        model = OrderItem
+admin.site.register(OrderItem,OrderItemAdmin)
--- store_kashpo/store/models.py
+++ store_kashpo/store/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     digital = models.BooleanField(default=False,null=True,blank=False)
+    discount = models.IntegerField(default=0,null=True,blank=False)
 
     def __str__(self):
         return self.name
@@ -44,6 +45,7 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
+    sent = models.BooleanField(default=False,null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
